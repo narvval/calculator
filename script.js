75379eea1b9d886db5cb8fd41bafc485e0e9dad3
narvval <nicolastordini@gmail.com>
2024-01-24 09:53:37 -0300

    Add initial styling

---
+++ script.js
@@ -0,0 +1,15 @@
+// TO-DO
+
+// Add function for add
+// Add function for substract
+// Add function for multiply
+// Add function for divide
+
+let num1;
+let operator;
+let num2;
+let display;
+
+function operate(num1, operator, num2) {
+
+}
