3bf565f922735899c5df1d228f061b0bf376467e
narvval <nicolastordini@gmail.com>
2024-01-28 09:51:45 -0300

    Fix bug: delete button making operations fail when applied on result

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// TO-DO
+// Add “backspace” button functionality
+// Fix 'operator' double-click returning NaN
+// Add % functionality
+// Add button animations (i.e., look pressed when clicking)
+// Add title / styling to whole page
+
 let num1 = null;
 let operator = null;
 let num2 = null;
@@ -10,8 +17,9 @@ let numButtons = document.querySelectorAll('.number');
 let operatorButtons = document.querySelectorAll('.operator');
 let equalButton = document.querySelector('.equals');
 let clearButton = document.querySelector('.clear');
+let deleteButton = document.querySelector('.delete');
 let zeroDisplay = document.querySelector('.zero-division-message');
-let decimalButton = document.querySelector('.decimal')
+let decimalButton = document.querySelector('.decimal');
 
 numButtons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -83,9 +91,29 @@ clearButton.addEventListener('click', () => {
     num2 = null;
     input = null;
     history = null;
+    result = null;
     displayResult.innerText = '';
     displayHistory.innerText = '';
     zeroDisplay.innerText = '';
+})
+
+deleteButton.addEventListener('click', () => {
+    if (result) {
+        console.log('case 1')
+        if (result) {
+            result = parseFloat(result.toString().slice(0, -1));
+            if (isNaN(result)) result = 0;
+            displayResult.innerText = result;
+            num1 = null;
+            input = result;
+            num2 = null;
+        }
+    }
+    else if (input) {
+        console.log('elseif')
+        input = input.slice(0, -1);
+        displayResult.innerText = input;
+    }
 })
 
 decimalButton.addEventListener('click', () => {
